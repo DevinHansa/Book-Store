235afa8fc4c5825eb7de0af1516c44e24fe548d3
DevinHansa <devinhansa@gmail.com>
2023-12-12 16:05:35 +0530

    database connect

---
+++ config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://as2020323:bYpCJt0dddDvPLR1 @bookstore.gtlalqa.mongodb.net/?retryWrites=true&w=majority';
+
+//bYpCJt0dddDvPLR1 //as2020323
+
+//mongodb+srv://as2020323:<password>@bookstore.gtlalqa.mongodb.net/?retryWrites=true&w=majority
